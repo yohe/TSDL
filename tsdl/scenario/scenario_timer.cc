28e493c8a107ec97827f96b59aa7710134df677d
Yohei.Sekiguchi <sekiguchi@Sirius.local>
2012-06-10 16:31:43 +0900

    Timer-class add.

---
+++ tsdl/scenario/scenario_timer.cc
@@ -0,0 +1,54 @@
+
+#include "tsdl/scenario/scenario_timer.h"
+using namespace boost::posix_time;
+
+ptime Timer::now() const {
+    return second_clock::local_time();
+}
+
+CountUpTimer::CountUpTimer() {
+}
+
+void CountUpTimer::set() {
+    setTime_ = now();
+    pause_ = false;
+}
+
+time_duration CountUpTimer::elapsed() const {
+    if(!isPause())
+        return (now() - setTime_);
+    else
+        return (pauseTime_ - setTime_);
+}
+
+time_duration CountUpTimer::pause() {
+    time_duration elapsedPTime(0,0,0,0);
+    if(!isPause()){
+        pauseTime_ = now();
+    } else {
+        elapsedPTime = now() - pauseTime_;
+        std::cout << elapsedPTime.total_seconds() << std::endl;
+        setTime_ += elapsedPTime;
+    }
+    pause_ = !pause_;
+    return elapsedPTime;
+}
+
+void CountDownTimer::set(long milisec){
+    setTime_ = (countUpTimer.now().time_of_day() + milliseconds(milisec));
+    countUpTimer.set();
+}
+
+long CountDownTimer::remainingTime(void) const {
+    time_duration result = setTime_ - countUpTimer.elapsed();
+    return (result.is_negative() ? 0 : result.total_milliseconds());
+}
+
+bool CountDownTimer::checkTimeOver(void) const {
+    return (remainingTime() > 0);
+}
+
+time_duration CountDownTimer::pause(void){
+    return countUpTimer.pause();
+}
+
---
+++ tsdl/scenario/scenario_timer.h
@@ -0,0 +1,114 @@
+#ifndef TSDL_SCENARIO_SCENARIO_TIMER_H
+#define TSDL_SCENARIO_SCENARIO_TIMER_H
+
+#include <boost/date_time.hpp>
+using namespace boost::posix_time;
+
+class Timer {
+public:
+    
+    Timer() {}
+    virtual ~Timer() {}
+
+    ptime now() const;
+
+private:
+
+};
+
+class CountUpTimer : public Timer {
+
+    /**
+     * カウントアップタイマ
+     * set()後からの経過時間を測る機能
+     */
+
+public:
+    CountUpTimer();
+
+    /**
+     * カウントアップを開始します
+     */
+    void set();
+
+    /**
+     * 経過時間をミリ秒で返します
+     * @return 経過時間
+     */
+    time_duration elapsed() const; 
+
+    /**
+     * カウントアップを一時停止します or 再開します
+     * @return Pauseしていた時間間隔を返します
+     */
+    time_duration pause();
+
+    /**
+     * Pause中かどうかを返します
+     * @return true 停止状態 / false 動作状態
+     */
+    bool isPause() const {
+        return pause_;
+    }
+
+protected:
+    bool pause_;
+    // 設定時刻
+    ptime setTime_;
+    // 一時停止指定時間
+    ptime pauseTime_;
+};
+
+
+class CountDownTimer{
+
+    /**
+     * カウントダウンタイマ
+     * 指定した長さの時間でカウントダウンします
+     * 
+     */
+
+public:
+    CountDownTimer();
+
+    /**
+     * カウントダウンの時間のを指定します。
+     * @param Time 測る時間を設定(単位は秒) 例）10.5秒後であれば 10.5を指定
+     */
+    void set(long milisec);
+
+    /**
+     * 残りの時間を取得します
+     * @return 残り時間
+     */
+    long remainingTime(void) const;
+
+    /**
+     * 指定時間を経過いないかを返します
+     * @return true 経過していない / false 経過している 
+     */
+    bool checkTimeOver(void) const;
+
+    /**
+     * カウントダウンを一時停止 or 再開します。
+     * @return Pause期間の時間
+     */
+    time_duration pause(void);
+
+    /**
+     * Pause中かどうかを返します。
+     * @return true 停止状態 / false 動作状態
+     */
+    bool isPause(void) const {
+        return countUpTimer.isPause();
+    }
+
+    ptime now() const { return countUpTimer.now(); }
+
+protected:
+    time_duration setTime_;
+    ptime pauseTime_;
+    CountUpTimer countUpTimer;
+};
+
+#endif
